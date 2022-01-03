ddf65b800940b0e981607e8c0415a8fe742084e3
hoangg <daoleviethoang@gmail.com>
2022-01-03 23:27:46 +0700

    Refacetor payload type

--- src/pages/profile-page/won-products/won-products.tsx
+++ src/pages/profile-page/won-products/won-products.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Spinner, Table } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useAppDispatch, useAppSelector } from "../../../app/hook";
-import { CancelDealPayload, requestWonList, cancelDeal } from "../../../app/reducers/user-slice";
+import { WinnerPayload, requestWonList, cancelDeal } from "../../../app/reducers/user-slice";
 import { RootState } from "../../../app/store";
 import { ConfirmModal2 } from "../../../components/common/confirm-modal-2/confirm-modal-2";
 import { apiRoute } from "../../../constants";
@@ -39,7 +39,7 @@ export const WonProducts = () => {
 
     const onCancelDeal = () => {
         console.log("Cancel deal");
-        const payload: CancelDealPayload = {
+        const payload: WinnerPayload = {
             productId: currentProduct,
             bidderId: currentBidder,
         };
